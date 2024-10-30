b66916b05a6099e4b7873346ec08c1d03532240f
Natan Campos <nasocabr@gmail.com>
2024-10-30 20:34:02 -0300

    lançando no GH

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de Rota</title>
+</head>
+<body>
+<h1>Esta página é para testar uma rota</h1>
+</body>
+</html>
